ba0531c65397ca50ea60ec8c926d463863c2b244
DESKTOP-DM517T5\farso <farsofli@gmail.com>
2018-03-12 13:30:20 +0100

    Added EqualIgnoreCase

--- src/main/java/com/example/demo/Model.java
+++ src/main/java/com/example/demo/Model.java
@@ -9,7 +9,7 @@ public class Model {
         user.setUsername("SANDRA");
         user.setPassword("RFB");
 
-        if (user.getUsername().equalsIgnoreCase(username) && user.getPassword().equals(password)) {
+        if (user.getUsername().equalsIgnoreCase(username) && user.getPassword().equalsIgnoreCase(password)) {
             return user;
         } return null;
 
